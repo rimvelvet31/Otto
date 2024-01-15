1d7db5bf609c7db663bb493b9cef627c403ab003
rimvelvet31 <redimportante13@gmail.com>
2024-01-15 12:01:05 +0800

    updated identifier rules

--- src/otto.py
+++ src/otto.py
@@ -94,7 +94,12 @@ class Lexer:
     def make_word(self):
         word = ""
 
-        # Identifier rules
+        # IDENTIFIER RULES
+        # - Identifiers are case sensitive
+        # - They cannot contain whitespace
+        # - They cannot contain special chars, expect underscore (_)
+        # - They can contain numbers, but are not allowed to start with one
+        # - They must start with an uppercase or lowercase letter, or with underscore
         identifier_regex = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
 
         while self.current_char is not None:
